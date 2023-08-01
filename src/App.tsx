691644ebc88d029e6ea4206e1a02058c13e6af3c
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-08-01 18:42:18 -0300

    feat: configurate axios and react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { Router } from "./Router";
 
+const queryClient = new QueryClient();
+
 export function App() {
-  return <Router />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
+  );
 }
