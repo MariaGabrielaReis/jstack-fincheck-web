abc13114a554d6ff0926e1c8a5ef93853ee93fc6
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-08-01 17:09:25 -0300

    feat: show input error and merge classnames

--- src/ui/components/Button.tsx
+++ src/ui/components/Button.tsx
@@ -1,12 +1,16 @@
 import { ComponentProps } from "react";
+import { cn } from "../../app/utils/cn";
 
 interface ButtonProps extends ComponentProps<"button"> {}
 
-export function Button(props: ButtonProps) {
+export function Button({ className, ...props }: ButtonProps) {
   return (
     <button
       {...props}
-      className="bg-teal-700  px-6 h-12 rounded-lg font-medium text-white disabled:text-gray-400 disabled:bg-gray-100 disabled:cursor-not-allowed hover:bg-teal-600 active:bg-teal-800 transition-all"
+      className={cn(
+        "bg-teal-700  px-6 h-12 rounded-lg font-medium text-white disabled:text-gray-400 disabled:bg-gray-100 disabled:cursor-not-allowed hover:bg-teal-600 active:bg-teal-800 transition-all",
+        className,
+      )}
     />
   );
 }
--- src/ui/components/Input.tsx
+++ src/ui/components/Input.tsx
@@ -1,11 +1,14 @@
+import { CrossCircledIcon } from "@radix-ui/react-icons";
 import { ComponentProps, forwardRef } from "react";
+import { cn } from "../../app/utils/cn";
 
 interface InputProps extends ComponentProps<"input"> {
   name: string;
+  error?: string;
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ placeholder, name, id, ...props }, ref) => {
+  ({ placeholder, name, id, error, className, ...props }, ref) => {
     const inputId = id ?? name;
 
     return (
@@ -16,15 +19,25 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
           name={name}
           id={inputId}
           placeholder=" "
-          className="w-full bg-white rounded-lg border border-teal-700 px-3 h-14 text-gray-800  placeholder-shown:border-none peer"
+          className={cn(
+            "w-full bg-white rounded-lg border border-teal-700 px-3 h-14 text-gray-800 focus:border-gray-800 placeholder-shown:border-none peer",
+            error && "!border-red-700 placeholder-shown:border-solid",
+            className,
+          )}
         />
         <label
           htmlFor={inputId}
-          // className="absolute left-3 top-3.5 pointer-events-none"
           className="absolute left-3 top-[-12px] pointer-events-none bg-gray-50 leading-4 px-1 peer-placeholder-shown:top-5 peer-placeholder-shown:bg-transparent peer-placeholder-shown:px-0 transition-all"
         >
           {placeholder}
         </label>
+
+        {error && (
+          <div className="flex items-center gap-1 mt-2 text-red-700">
+            <CrossCircledIcon />
+            <span className="text-sm">{error}</span>
+          </div>
+        )}
       </div>
     );
   },
