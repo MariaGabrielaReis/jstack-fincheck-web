6b7a2b11df6c75d86a5f11d7c97ef7518b9a4874
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-08-01 18:44:04 -0300

    feat: create signup service

---
+++ src/app/services/authService/index.ts
@@ -0,0 +1,3 @@
+import { signUp } from "./signup";
+
+export const authService = { signUp };
---
+++ src/app/services/authService/signup.ts
@@ -0,0 +1,10 @@
+import { api } from "../httpClient";
+
+export type SignUpParams = { name: string; email: string; password: string };
+
+export type SignUpResponse = { accessToken: string };
+
+export async function signUp(params: SignUpParams) {
+  const { data } = await api.post<SignUpResponse>("/auth/signup", params);
+  return data;
+}
