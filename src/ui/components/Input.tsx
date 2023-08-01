18f942a9118481fd2f68d6a0b93df50092713a39
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-08-01 16:41:39 -0300

    feat: add input foward ref

--- src/ui/components/Input.tsx
+++ src/ui/components/Input.tsx
@@ -1,28 +1,33 @@
-import { ComponentProps } from "react";
+import { ComponentProps, forwardRef } from "react";
 
 interface InputProps extends ComponentProps<"input"> {
   name: string;
 }
 
-export function Input({ placeholder, name, id, ...props }: InputProps) {
-  const inputId = id ?? name;
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ placeholder, name, id, ...props }, ref) => {
+    const inputId = id ?? name;
 
-  return (
-    <div className="relative">
-      <input
-        {...props}
-        name={name}
-        id={inputId}
-        placeholder=" "
-        className="w-full bg-white rounded-lg border border-teal-700 px-3 h-14 text-gray-800  placeholder-shown:border-none peer"
-      />
-      <label
-        htmlFor={inputId}
-        // className="absolute left-3 top-3.5 pointer-events-none"
-        className="absolute left-3 top-[-12px] pointer-events-none bg-gray-50 leading-4 px-1 peer-placeholder-shown:top-5 peer-placeholder-shown:bg-transparent peer-placeholder-shown:px-0 transition-all"
-      >
-        {placeholder}
-      </label>
-    </div>
-  );
-}
+    return (
+      <div className="relative">
+        <input
+          {...props}
+          ref={ref}
+          name={name}
+          id={inputId}
+          placeholder=" "
+          className="w-full bg-white rounded-lg border border-teal-700 px-3 h-14 text-gray-800  placeholder-shown:border-none peer"
+        />
+        <label
+          htmlFor={inputId}
+          // className="absolute left-3 top-3.5 pointer-events-none"
+          className="absolute left-3 top-[-12px] pointer-events-none bg-gray-50 leading-4 px-1 peer-placeholder-shown:top-5 peer-placeholder-shown:bg-transparent peer-placeholder-shown:px-0 transition-all"
+        >
+          {placeholder}
+        </label>
+      </div>
+    );
+  },
+);
+
+Input.displayName = "Input";
