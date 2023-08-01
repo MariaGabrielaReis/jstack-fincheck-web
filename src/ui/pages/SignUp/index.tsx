9f2e0611346a247e4264d125c0ffbcb4a78c4852
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-08-01 17:13:47 -0300

    feat: validate signup form

--- src/ui/pages/SignUp/index.tsx
+++ src/ui/pages/SignUp/index.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
+import { useSignUpController } from "./useSignUpController";
 
 export function SignUp() {
+  const { register, handleSubmit, errors } = useSignUpController();
+
   return (
     <>
       <header className="flex flex-col items-center gap-4">
@@ -23,10 +26,24 @@ export function SignUp() {
         </p>
       </header>
 
-      <form className="mt-16 flex flex-col gap-6">
-        <Input name="nome" type="nome" placeholder="Nome" />
-        <Input name="email" type="email" placeholder="E-mail" />
-        <Input name="password" type="password" placeholder="Senha" />
+      <form onSubmit={handleSubmit} className="mt-16 flex flex-col gap-6">
+        <Input
+          placeholder="Nome"
+          error={errors.name?.message}
+          {...register("name")}
+        />
+        <Input
+          type="email"
+          placeholder="E-mail"
+          error={errors.email?.message}
+          {...register("email")}
+        />
+        <Input
+          type="password"
+          placeholder="Senha"
+          error={errors.password?.message}
+          {...register("password")}
+        />
 
         <Button type="submit" className="mt-2">
           Criar conta
---
+++ src/ui/pages/SignUp/useSignUpController.ts
@@ -0,0 +1,31 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  name: z.string().nonempty("Nome é obrigatório"),
+  email: z
+    .string()
+    .nonempty("E-mail é obrigatório")
+    .email("Informe um e-mail válido"),
+  password: z
+    .string()
+    .nonempty("Senha é obrigatória")
+    .min(8, "A senha deve conter ao menos 8 caracteres"),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export function useSignUpController() {
+  const {
+    register,
+    handleSubmit: hookFormHandleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const handleSubmit = hookFormHandleSubmit(data => {
+    console.log({ data });
+  });
+
+  return { register, handleSubmit, errors };
+}
