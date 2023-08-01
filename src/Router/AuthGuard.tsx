080281f7fc1d6fa4fe2dfeb324f335308d6ec141
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-08-01 19:25:46 -0300

    feat: use auth context

--- src/Router/AuthGuard.tsx
+++ src/Router/AuthGuard.tsx
@@ -1,11 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../app/hooks/useAuth";
 
 export interface AuthGuardProps {
   isPrivate?: boolean;
 }
 
 export function AuthGuard({ isPrivate = false }: AuthGuardProps) {
-  const signedIn = false;
+  const { signedIn } = useAuth();
 
   if (isPrivate && !signedIn) return <Navigate to="/signin" replace />;
   if (!isPrivate && signedIn) return <Navigate to="/" replace />;
