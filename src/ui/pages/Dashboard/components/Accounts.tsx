370bfe8794ab41a771e597ec11090d2e18b2df2e
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-08-04 19:23:11 -0300

    feat: create account list component

---
+++ src/ui/pages/Dashboard/components/Accounts.tsx
@@ -0,0 +1,46 @@
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { EyeIcon } from "../../../components/icons/EyeIcon";
+import { AccountCard } from "./AccountCard";
+import { AccountsSliderNavigation } from "./AccountsSliderNavigation";
+
+export function Accounts() {
+  return (
+    <div className="bg-teal-700 rounded-2xl w-full h-full px-4 py-8 md:p-10 flex flex-col">
+      <div>
+        <span className="tracking-[-0.5px] text-white block">Saldo total</span>
+
+        <div className="flex items-center gap-2">
+          <strong className="text-2xl tracking-[-1px] text-white">
+            R$ 100.000,00
+          </strong>
+          <button className="w-8 h-8 flex items-center justify-center">
+            <EyeIcon open={true} />
+          </button>
+        </div>
+      </div>
+
+      <div className="flex-1 flex flex-col justify-end">
+        <div>
+          <Swiper spaceBetween={16} slidesPerView={2.2}>
+            <div
+              slot="container-start"
+              className="flex items-center justify-between mb-2"
+            >
+              <span className="tracking-[-1px] text-white text-lg">
+                Minhas contas
+              </span>
+
+              <AccountsSliderNavigation />
+            </div>
+
+            <SwiperSlide>
+              <AccountCard color="#045caa" name="BTG" balance={7000.21} />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
+}
