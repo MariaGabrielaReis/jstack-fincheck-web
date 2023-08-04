4ce04094693aff8d16695dfca0c56f09bbbd88bf
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-08-04 17:55:03 -0300

    feat: create user avatar component

---
+++ src/ui/components/UserAvatar.tsx
@@ -0,0 +1,9 @@
+export function UserAvatar() {
+  return (
+    <div className="bg-teal-700 rounded-full w-12 h-12 flex items-center justify-center">
+      <span className="text-sm tracking-[-0.5px] font-medium text-teal-50">
+        MG
+      </span>
+    </div>
+  );
+}
