7cd19461847cdf4923ffb6429050d1bf8ee4724f
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-08-04 19:00:36 -0300

    feat: add more icons

--- src/ui/components/icons/categories/CategoryIcon.tsx
+++ src/ui/components/icons/categories/CategoryIcon.tsx
@@ -2,15 +2,11 @@ import { iconsMap } from "./iconsMap";
 
 interface CategoryIconProps {
   type: "income" | "expense";
-  category?: string;
+  category?: keyof (typeof iconsMap.expense | typeof iconsMap.income);
 }
 
-type Options = keyof (typeof iconsMap.expense | typeof iconsMap.income);
-
 export function CategoryIcon({ type, category }: CategoryIconProps) {
-  const Icon =
-    iconsMap[type][(category as Options) ?? "default"] ??
-    iconsMap[type].default;
+  const Icon = iconsMap[type][category ?? "default"] ?? iconsMap[type].default;
 
   return <Icon />;
 }
